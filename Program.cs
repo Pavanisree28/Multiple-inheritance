6d912401777a9bc128a88e26bf0970563989320f
shine <shine@PAVANISREE>
2026-06-01 15:15:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Multiple_inheritance
+{
+    interface IphoneBasic
+    {
+        void Calling();
+        void Receiving();
+        void Sendmessage();
+        void Endmessage();
+
+    }
+    class Iphonese : IphoneBasic
+    {
+        public void Calling()
+        {
+            Console.WriteLine("IphoneSe is calling");
+        }
+        public void Receiving()
+        {
+            Console.WriteLine("IphoneSe is receiving");
+        }
+        public void Sendmessage()
+        {
+            Console.WriteLine("IphoneSe is sending message"); 
+        }
+        public void Endmessage()
+        {
+            Console.WriteLine("IphoneSe is ending message");
+        }
+        
+    }
+    interface IphoneAdavnced
+    {
+        void Faceid();
+        void Vediocalling();
+        void AirDrop();
+    }
+    class Iphon16 : IphoneBasic, IphoneAdavnced
+    {
+        public void Calling()
+        {
+            Console.WriteLine("Iphon16 is calling");
+        }
+        public void Receiving()
+        {
+            Console.WriteLine("Iphon16 is receiving");
+        }
+        public void Sendmessage ()
+        {
+            Console.WriteLine("Iphon16 is sending message");
+        }
+        public void Endmessage()
+        {
+            Console.WriteLine("Iphon16 is ending message");
+        }
+        public void Faceid()
+        {
+            Console.WriteLine("Iphon16 has faceid");
+        }
+        public void Vediocalling()
+        {
+            Console.WriteLine("Iphon16 has vediocalling");
+        }
+        public void AirDrop()
+        {
+            Console.WriteLine("Iphon16 has AirDrop");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IphoneBasic obj=new Iphonese();
+            obj.Calling();
+            obj.Receiving();
+            obj.Sendmessage();
+            obj.Endmessage();
+            obj=new Iphon16();
+            obj.Calling();
+            obj.Receiving();
+            obj.Sendmessage();
+            obj.Endmessage();
+            IphoneAdavnced obj1=new Iphon16();
+            obj1.Faceid();
+            obj1.Vediocalling();
+            obj1.AirDrop();
+
+        }
+    }
+}
